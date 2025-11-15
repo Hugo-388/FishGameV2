f3d25edcae18d66006d0b1eca87dcd32a8ccf706
Hugo-3_8 <hugomx@sfr.fr>
2025-11-15 20:33:33 +0100

    feat: Ajout des sprites

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
-﻿using var game = new FishGameV2.Game1();
+﻿using var game = new FishGameV2.myGame();
 game.Run();
--- myGame.cs
+++ myGame.cs
@@ -4,12 +4,12 @@ using Microsoft.Xna.Framework.Input;
 
 namespace FishGameV2;
 
-public class Game1 : Game
+public class myGame : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
 
-    public Game1()
+    public myGame()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
